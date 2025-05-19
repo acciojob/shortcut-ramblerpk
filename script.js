72782c2161f317c76ccca8bb656f191701d63e48
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-05-19 16:20:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-function shortcut(s1, s2) {
-  // your code here
-	if (str1.isEmpty() || str2.isEmpty()) {
-        return ""; // Return empty string if any input is empty
-    }
-    // Get the first character of each string
-    char firstChar1 = str1.charAt(0);
-    char firstChar2 = str2.charAt(0);
-    
-    // Return the concatenated result
-    return "" + firstChar1 + firstChar2; 
-	
+function shortcut(str1, str2) {
+  if (!str1 || !str2) return '';
+  return str1[0] + str2[0];
 }
+
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
+
 
